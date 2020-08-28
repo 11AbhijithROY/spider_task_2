9f310b9954cda443d72cad25bd27aeb1bf52a584
11AbhijithROY <roy.abhijith11@gmail.com>
2020-08-28 17:15:07 +0300

    added buttons

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var score = 0;
 var totQuestions = questions.length;
 var answer = [];
 var seconds = 0;
-var minutes = 2;
+var minutes = 5;
 var starttime;
 //var wrongans = [];
 
@@ -39,17 +39,20 @@ function timer() {
 }
 
 function loadQuiz() {
+    //to remove a selected option that was not submitted from a previous test
     var selectedOption = document.querySelector("input[type=radio]:checked");
     if(selectedOption) {
         selectedOption.checked = false;
     }
+
+    loadbuttons();
     startscr.style.display = "none";
     container.style.display = "";
     startime = setInterval(timer,1000);
     loadQuestion(currentQuestion);
 }
 function loadQuestion (index) {
-    if(index != totQuestions - 1) {
+    /*if(index != totQuestions - 1) {
         nextButton.textContent = "Next";
     }
     if(index == 0){
@@ -57,8 +60,19 @@ function loadQuestion (index) {
     }
     else 
         prevButton.style.display = "";
-    var q = questions[index];
-    questionEl.textContent = (index + 1) + '. ' + q.question; 
+    */
+    //to load the question and options from questions.js
+    
+    currentQuestion = index;
+    console.log(currentQuestion);
+    if(typeof(index) != "string") {
+        var q = questions[index];
+        questionEl.textContent = (index + 1) + '. ' + q.question;
+    }
+    else {
+        var q = questions[index];
+        questionEl.textContent = (index) + '. ' + q.question;
+    } 
     opt1.textContent = q.option1;
     opt2.textContent = q.option2;
     opt3.textContent = q.option3;
@@ -75,7 +89,52 @@ function scoring () {
     result.textContent = 'Hey ' + playername + '! Your Score is ' + score + '/150';
     result.style.display = "";
 }
-function loadNextQuestion () {
+function loadbuttons () {
+    //to choose any question
+    var ques_btn;
+    var ques_btncont = document.createElement("div");
+    ques_btncont.setAttribute("class","ques_btncont");
+        for(let i = 0;i < totQuestions;i++) {
+        ques_btn = document.createElement("button");
+        ques_btn.textContent = i+1;
+        ques_btn.setAttribute("class","ques_btn");
+        ques_btn.value = i;
+        ques_btn.addEventListener("click", function(){
+            var quesNO = this.value;
+            //currentQuestion = quesNO;
+            console.log('this is ' + this.value)
+            loadQuestion(quesNO);
+        })
+        ques_btncont.appendChild(ques_btn);
+    }
+    container.appendChild(ques_btncont);
+}
+function submitQuestion () {
+    //to make sure an option is selected before 'submit'
+    var selectedOption = document.querySelector("input[type=radio]:checked");
+    if(!selectedOption){
+        alert("PLEASE SELECT AN OPTION!");
+        return;
+    }
+
+    //selected option is stored in answer array
+    answer[currentQuestion] = selectedOption.value; 
+    //returned to no selected option for the next question
+    selectedOption.checked = false;
+    currentQuestion++;
+    if(currentQuestion == totQuestions - 1){
+        nextButton.textContent = "Finish";
+    }
+    if(currentQuestion == totQuestions) {
+        container.style.display = "none";
+        //timer stopped when quiz is over
+        seconds = 0;
+        minutes = 0;
+        return;
+    }
+    loadQuestion(currentQuestion);
+}
+/*function loadNextQuestion () {
     var selectedOption = document.querySelector("input[type=radio]:checked");
     if(!selectedOption){
         alert("PLEASE SELECT AN OPTION!");
@@ -97,7 +156,7 @@ function loadNextQuestion () {
     loadQuestion(currentQuestion);
 }
 
-function loadPrevQuestion () {
+/*function loadPrevQuestion () {
     var selectedOption = document.querySelector("input[type=radio]:checked");
     if(selectedOption){
     selectedOption.checked = false;
@@ -105,4 +164,4 @@ function loadPrevQuestion () {
     currentQuestion--;
     loadQuestion(currentQuestion);
 
-}
+}*/
